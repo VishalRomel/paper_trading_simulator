a521aad6d70644eb4a0af39ae4f10e9916485123
roomfulmoshe <moshe@roomful.co>
2023-10-03 18:58:20 -0400

    Post method - arbitrary ticker
    Arbitrary ticker can be sent to enpoint

--- stock_endpoints.js
+++ stock_endpoints.js
@@ -5,16 +5,22 @@ const axios = require('axios');
 
 const app = express();
 
+app.use(express.json());
+
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500"); 
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     
     next(); 
   });
 
 const apiKey = 'ALVUPDZ6TOR4O1DV';
 const symbol = 'AAPL';
 
-app.get('/av', async (req, res) => {
-    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=${symbol}&apikey=${apiKey}&timeseries.last=1`;
+app.post('/av', async (req, res) => {
+    console.log(req.body);
+    const stock = req.body.stock;
+    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=${stock}&apikey=${apiKey}&timeseries.last=1`;
 
     axios
       .get(url, {
@@ -32,10 +38,15 @@ app.get('/av', async (req, res) => {
             const recentWeeks = sortedDates.slice(0, 52);
             const recentPrices = recentWeeks.map(date => response[date]);
 
-            console.log(sortedDates);
-            console.log(Object.keys(recentWeeks));
-            console.log(recentPrices);
-            res.json(recentPrices);
+            const chartData = recentWeeks.map((week, i) => {
+                return {
+                  week: week, 
+                  price: recentPrices[i]['4. close'] 
+                }
+              })
+
+            console.log(chartData);
+            res.json(chartData);
         }
       })
       .catch((error) => {
